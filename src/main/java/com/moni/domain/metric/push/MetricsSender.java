37ba8bb9c6a03aa29162c1aaacb730226b3d02c4
윤준석 <ujs9901@naver.com>
2026-08-15 18:10:26 +0900

    refactor: collectedAt 값을 숫자 값이 아닌 문자열로 직렬화하도록 수정

--- src/main/java/com/moni/domain/metric/push/MetricsSender.java
+++ src/main/java/com/moni/domain/metric/push/MetricsSender.java
@@ -1,5 +1,7 @@
 package com.moni.domain.metric.push;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.moni.domain.metric.dto.request.MetricsPayload;
 import com.moni.global.config.MoniProperties;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +9,8 @@ import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
@@ -23,9 +27,16 @@ public class MetricsSender {
         ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.defaults()
                 .withConnectTimeout(properties.getConnectTimeout())
                 .withReadTimeout(properties.getReadTimeout());
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
         this.restClient = RestClient.builder()
                 .baseUrl(properties.getServerUrl())
                 .requestFactory(ClientHttpRequestFactoryBuilder.detect().build(settings))
+                .messageConverters(converters -> converters.stream()
+                        .filter(MappingJackson2HttpMessageConverter.class::isInstance)
+                        .map(MappingJackson2HttpMessageConverter.class::cast)
+                        .forEach(converter -> converter.setObjectMapper(objectMapper)))
                 .build();
     }
 
